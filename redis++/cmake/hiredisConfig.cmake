fd39fc34d461a8c5aca8353a696ccc00e163bdde
pan-shile <3502173090@qq.com>
2026-07-17 14:26:28 +0800

    chore: 清理项目中废弃的示例代码与测试文件
    移除了多个模块的示例、测试代码以及相关配置文件，包括qq邮箱示例、gflags、odb、redis++、spdlog、bRpc、websocketpp、etcd、gtest相关的测试和示例文件，以及对应的构建配置文件。

--- redis++/cmake/hiredisConfig.cmake
+++
@@ -1,9 +0,0 @@
-# Minimal hiredis config to satisfy redis++ dependency check
-find_path(HIREDIS_INCLUDE_DIR hiredis/hiredis.h REQUIRED)
-find_library(HIREDIS_LIB hiredis REQUIRED)
-
-add_library(hiredis::hiredis UNKNOWN IMPORTED)
-set_target_properties(hiredis::hiredis PROPERTIES
-    IMPORTED_LOCATION "${HIREDIS_LIB}"
-    INTERFACE_INCLUDE_DIRECTORIES "${HIREDIS_INCLUDE_DIR}"
-)
